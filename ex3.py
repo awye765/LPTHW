7c1c06ca2923fcd4a0e9d960c36e94bad1f05a12
awye765 <awye765@googlemail.com>
2016-11-06 19:23:34 +0000

    Initial commit. Ex3 complee

---
+++ ex3.py
@@ -0,0 +1,14 @@
+# My first experiments with python!
+
+print "Hello world!"  # => nil
+5 / 2                 # => 2
+5.0 / 2.0             # => 2.5
+5 % 2                 # => 1
+5 / 3                 # => 1
+5 / 3                 # => 1
+-5 // 3               # => -2
+# The FLOOR function maps a real number to the LARGEST PREVIOUS integer.  The
+# CEILING function maps a real number to the SMALLEST FOLLOWING integer.  In
+# python division uses FLOOR DIVISION. Therefore 5 // 3 means the result is -2
+# (i.e. the largest previous integer after the actual number answer,
+# 1.66666666...).
---
+++ test.py
@@ -0,0 +1,10 @@
+# My first experiments with python!
+
+print "Hello world!"  # => nil
+5 / 2                 # => 2
+5.0 / 2.0             # => 2.5
+5 % 2                 # => 1
+5 / 3                 # => 1
+5 /3                  # => 1
+
+# >> Hello world!
